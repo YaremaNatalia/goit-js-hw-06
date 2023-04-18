2fbe462a6b5b87941d4a28bf79480001b7da56ce
YaremaNatalia <yaremka.n@gmail.com>
2023-04-18 23:10:38 +0300

    add fix foontSize

--- js/task-07.js
+++ js/task-07.js
@@ -1,14 +1,28 @@
 const ref = {
   input: document.querySelector("#font-size-control"),
-  text: document.getElementById('text'),
+  text: document.getElementById("text"),
 };
+
+ref.text.style.fontSize = `${ref.input.value}px`;  //щоб не було скачка тексту
 
 ref.input.addEventListener("input", onChangeFontSize);
 
+// !============================
+// function onChangeFontSize (e){
+//     const fontSize = e.target.value;
 
-function onChangeFontSize (e){
-    const fontSize = e.target.value;
+//     ref.text.style.fontSize = fontSize + "px";
+// }
+// !============================
+// function onChangeFontSize(e) {
+//   const fontSize = e.target.value;
 
-    ref.text.style.fontSize = fontSize + "px";
+//   ref.text.style.fontSize = `${fontSize}px`;
+// }
+// !============================
+function onChangeFontSize(e) {
+  const fontSize = e.target.value;
+
+  ref.text.style.fontSize = `${fontSize}px`;
 }
 
