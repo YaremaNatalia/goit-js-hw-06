617c041947b19724b3daa93a484f5c0a0f1ab424
YaremaNatalia <yaremka.n@gmail.com>
2023-04-21 01:43:29 +0300

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -10,11 +10,9 @@ function onCheckInputValidation(event) {
   const lengthValue = event.target.value.trim().length;
   if (lengthValue === Number(dataLength)) {
     ref.input.classList.add("valid");
-    ref.input.classList.remove("invalid");
+    ref.input.classList.remove("invalid"); // обовязково видалити протилежний клас для коректного функціонування
   } else {
     ref.input.classList.add("invalid");
-    ref.input.classList.remove("valid");
+    ref.input.classList.remove("valid"); // обовязково видалити протилежний клас для коректного функціонування
   }
 }
-
-
